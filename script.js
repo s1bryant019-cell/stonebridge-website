1c8b776facac0ab2b658a33e9941ec73fcbc0a1b
s1bryant019-cell <s1bryant019@gmail.com>
2026-04-20 20:28:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+document.querySelectorAll('.mobile-toggle').forEach((button) => {
+  button.addEventListener('click', () => {
+    const menu = document.querySelector('.menu');
+    if (menu) menu.classList.toggle('open');
+  });
+});
 function submitDemoForm(event) {
   event.preventDefault();
   alert('This is a mockup form for now. It can be connected to a real contact form later.');
