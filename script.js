ed123324a6ce6da5c4f1a396d0c57f295d3cc9a3
s1bryant019-cell <s1bryant019@gmail.com>
2026-09-23 16:38:14 -0500

    Refresh mobile profile hero asset version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-  var VERSION = '20260923mobile4';
+  var VERSION = '20260923mobile5';
 
   function pageName() {
     var path = (window.location.pathname || '').split('/').pop().toLowerCase();
