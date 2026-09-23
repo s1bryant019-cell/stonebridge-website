2d4f606bf2b213f634f1fe7a1480f7e861ca5e62
s1bryant019-cell <s1bryant019@gmail.com>
2026-09-23 16:46:11 -0500

    Refresh mobile footer asset version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-  var VERSION = '20260923mobile5';
+  var VERSION = '20260923mobile6';
 
   function pageName() {
     var path = (window.location.pathname || '').split('/').pop().toLowerCase();
