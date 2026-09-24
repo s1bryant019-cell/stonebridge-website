2452fe8205f1f3d94aad7ea30ba390f9dbbe340e
s1bryant019-cell <s1bryant019@gmail.com>
2026-09-24 09:55:46 -0500

    Refresh header asset version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-  var VERSION = '20260924headerfix1';
+  var VERSION = '20260924headerfix2';
 
   function pageName() {
     var path = (window.location.pathname || '').split('/').pop().toLowerCase();
